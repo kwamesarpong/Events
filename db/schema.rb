592a8cc441e2305b4b639453a295214b8235e260
kwamesarpong <kayosarp@gmail.com>
2017-02-06 17:16:36 +0000

    Worked on listings and deleted some of the pictures

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170206105719) do
 
-  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20170206105719) do
     t.string   "category_pic"
   end
 
-  create_table "profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "profiles", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "subscription_id"
     t.string   "name_of_agency"
@@ -30,22 +30,22 @@ ActiveRecord::Schema.define(version: 20170206105719) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.string   "short_desc",      limit: 140
-    t.index ["subscription_id"], name: "index_profiles_on_subscription_id", using: :btree
-    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+    t.index ["subscription_id"], name: "index_profiles_on_subscription_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
-  create_table "reviews", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "reviews", force: :cascade do |t|
     t.integer  "service_id"
     t.integer  "user_id"
     t.string   "message"
     t.integer  "co_efficient"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["service_id"], name: "index_reviews_on_service_id", using: :btree
-    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
+    t.index ["service_id"], name: "index_reviews_on_service_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table "services", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "services", force: :cascade do |t|
     t.integer  "profile_id"
     t.integer  "category_id"
     t.string   "picture"
@@ -53,20 +53,20 @@ ActiveRecord::Schema.define(version: 20170206105719) do
     t.integer  "price"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["category_id"], name: "index_services_on_category_id", using: :btree
-    t.index ["profile_id"], name: "index_services_on_profile_id", using: :btree
+    t.index ["category_id"], name: "index_services_on_category_id"
+    t.index ["profile_id"], name: "index_services_on_profile_id"
   end
 
-  create_table "subcategories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "subcategories", force: :cascade do |t|
     t.integer  "category_id"
     t.string   "name"
     t.string   "sub_cat_image"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["category_id"], name: "index_subcategories_on_category_id", using: :btree
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
   end
 
-  create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "subscriptions", force: :cascade do |t|
     t.string   "name"
     t.integer  "amount"
     t.boolean  "recurring"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 20170206105719) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",           default: "info@eventicise.com", null: false
     t.string   "username"
     t.string   "password_digest"
