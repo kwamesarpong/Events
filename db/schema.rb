402514811e89ceba99b08f2fc14c0a9781e1cddf
bkwaku <bkwaku@rocketmail.com>
2017-02-14 16:31:54 +0000

    Add read or unread to message table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214102609) do
+ActiveRecord::Schema.define(version: 20170214163059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170214102609) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "sender"
+    t.boolean  "read"
     t.index ["mail_box_id"], name: "index_messages_on_mail_box_id", using: :btree
   end
 
